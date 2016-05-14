315d73a28929939597bb95d687b3a5268ed6f0c1
Martin LinuxVBox <marting12@live.dk>
2016-05-14 21:15:06 +0200

    Updates model relations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,4 @@
 class Lesson < ActiveRecord::Base
   belongs_to :topic
+  belongs_to :user, through => :topic
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   has_many :lessons
+  belongs_to :user
 end
