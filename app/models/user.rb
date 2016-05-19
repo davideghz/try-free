339e82d3daa827acd8d36e0dc31cc5f866e4a84d
Davide Ghezzi <davideghz@gmail.com>
2016-05-19 08:50:29 +0200

    fix through relationships syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :topics
-  has_many :lessons, through => :relationships
+  has_many :lessons, through: :relationships
 end
