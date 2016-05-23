4e55f802beb6de7e8765d9fdaed7a5a7de96209f
Martin LinuxVBox <marting12@live.dk>
2016-05-23 22:03:01 +0200

    Adds user_id to lesson table the referenced way

--- db/migrate/20160513164300_create_lessons.rb
+++ db/migrate/20160513164300_create_lessons.rb
@@ -2,7 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
       t.date :date
-      t.integer :user_id
+      t.reference :user, index: true, foreign_key: true
       t.integer :topic_id
 
       t.timestamps null: false
