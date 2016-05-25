386425db63887e9a8d4bf82edf8d9ac5335fcd60
Martin LinuxVBox <marting12@live.dk>
2016-05-25 14:30:21 +0200

    Updates topic controller with create action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
   def index
     @topics = Topic.all
   end
@@ -11,10 +12,24 @@ class TopicsController < ApplicationController
   end
 
   def create
+    @topic = Topics.new(topics_params)
 
+    respnd_to do |format|
+      if @topic.save
+        format.html {redirect_to @topic, flash: "Topic was successfully created"}
+        format.json { render :show, status: :created, location: @topic}
+      else
+        format.html {render :new}
+        format.json {render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
 
   def topics_params
     params.require(:topic).permit()
