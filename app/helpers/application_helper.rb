dd338f10babd1d91bf849b1729425c4795e6c30c
Davide Ghezzi <davideghz@gmail.com>
2016-05-27 17:05:21 -0400

    add i18n to the app and language switcher to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def alternate_locales
+    # Remove current locale from list
+    aval_locales_but_current = I18n.available_locales - [locale]
+    aval_locales_but_current.map{ |l| yield(l) }.join.html_safe
+  end
+
 end
