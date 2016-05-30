6992589a16ae522e37e7167555d4acc162e72b27
Martin LinuxVBox <marting12@live.dk>
2016-05-30 05:33:42 +0200

    Seeds database with user for Martin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "marting12@live.dk", password: "hejhej")
+User.create(email: "marting12@live.dk", password: "hejhejhej")
