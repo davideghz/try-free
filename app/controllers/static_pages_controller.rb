d4b0dedf22649800c8183d5e7c536f8f736b5c3b
Davide Ghezzi <davideghz@gmail.com>
2016-06-06 16:46:57 -0400

    add scopes to filter lessons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @available_lessons = Lesson.all.order(:lesson_date)
+    # @available_lessons = Lesson.all.order(:lesson_date)
+    @available_lessons = Lesson.filter(params.slice(:country, :contains)).order(:lesson_date)
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,13 @@
 class Lesson < ActiveRecord::Base
+
+  scope :country, -> (country) { where country: country }
+
+  scope :contains, ->(word) {
+    joins(:topic).where('topics.title LIKE ?', "%#{word.html_safe}%")
+  }
+
+  include Filterable
+
   belongs_to :topic, inverse_of: :lessons
   belongs_to :user
 
