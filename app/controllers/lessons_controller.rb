183bf3355b7761ed47923321d528a9598b978579
Davide Ghezzi <davideghz@gmail.com>
2016-06-06 23:49:49 -0400

    render dialog on lesson click on homepage

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,6 +5,10 @@ class LessonsController < ApplicationController
 
   def show
     @lesson = Lesson.find(params[:id])
+    respond_to do |format|
+      format.html {}
+      format.js
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   scope "(:locale)" do
     root 'static_pages#home'
-    get 'lessons/index'
-    get 'lessons/show'
+
+    resources :lessons, only: [:show]
 
     resources :topics, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
       resources :lessons, only: [:index, :show, :new, :create]
