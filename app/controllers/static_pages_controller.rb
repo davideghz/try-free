19e04ce6db30b6a2f58c29f4b6ebb166870d4098
Davide Ghezzi <davideghz@gmail.com>
2016-06-13 11:16:58 -0400

    hide past lesson from home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     # @available_lessons = Lesson.all.order(:lesson_date)
-    @available_lessons = Lesson.filter(params.slice(:country, :contains)).order(:lesson_date)
+    @available_lessons = Lesson.filter(params.slice(:country, :contains)).where('lesson_date >= ?', Date.today).order(:lesson_date)
   end
 end
