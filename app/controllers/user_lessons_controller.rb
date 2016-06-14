d0dc33288b0c4b4911d9d12819b82c9489a6af9f
Davide Ghezzi <davideghz@gmail.com>
2016-06-13 22:45:58 -0400

    show real subscribed users to lessons

--- app/controllers/user_lessons_controller.rb
+++ app/controllers/user_lessons_controller.rb
@@ -2,12 +2,14 @@ class UserLessonsController < ApplicationController
   def create
     lesson = Lesson.find(params[:lesson_id])
     current_user.subscribe(lesson)
+    flash[:success] = "You succesfully subscribed to the lesson!"
     redirect_to root_path
   end
 
   def destroy
     lesson = UserLesson.find(params[:id])
     current_user.unsubscribe(lesson)
+    flash[:success] = "You succesfully unsubscribed from the lesson!"
     redirect_to root_path
   end
 
