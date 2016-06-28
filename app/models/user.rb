8eedd3e827afe29d23cb6a6693e3797228eef24a
Martin LinuxVBox <marting12@live.dk>
2016-06-28 21:03:36 +0200

    Changes filter method for profile creation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   has_one :profile, dependent: :destroy
   # This after filter is Rails magic, default method from creating has_one relationship
-  after_create :build_profile
+  after_create :create_profile
 
   # Subscribe to a lesson.
   def subscribe(lesson)
